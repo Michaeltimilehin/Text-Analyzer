e16e7525a1fe8a6f53cc12600889d5c688864e36
Michaeltimilehin <adurogboyemichael9@gmail.com>
2024-07-17 21:39:47 +0100

    add javascript

--- js/script.js
+++ js/script.js
@@ -1,41 +1,7 @@
-function wordCounter(text) {
-  return 1
-
-
-}
-
-
 $(document).ready(function () {
-$("form").submit(function (event) {
-  event.preventDefault()
-    let sentence = $("#sentence").val()
-    let wordCount = wordCounter(text)
-    $("#output").text(wordCount)
+  $("#mark").click(function () {
+    $(".Michael").show()
+    $("#mark").hide()
   })
-
-  function wordCounter(text) {
-
-
-    if (!text.trim()) {
-      return 0
-    }
-    let counter = 0
-    let array = text.split(" ")
-    array.forEach(function (word) {
-      if (!Number(word)) {
-        counter++
-      }
-    })
-
-    return counter
-  }
-  $(document).ready(function () {
-    $("form").submit(function (event) {
-      event.preventDefault()
-      let sentence = $("#sentence").val()
-      console.log(sentence)
-      let wordCount = wordCounter(sentence)
-      $("#output").text(wordCount)
-    })
-  })
+  
 })
